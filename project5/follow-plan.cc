dbc9da82104a706630da662f517b0c61c10216c2
50MolesOfNaCl <edmund.lam2@gmail.com>
2020-11-12 14:07:59 -0800

    cleaned up indentation with VSC

--- project5/follow-plan.cc
+++ project5/follow-plan.cc
@@ -1,6 +1,6 @@
 /**
  * follow-plan.cc
- * 
+ *
  * Sample code for a robot that has two front bumpers and a laser, and
  * which is provided with localization data.
  *
@@ -9,14 +9,14 @@
  *
  * Written by: Simon Parsons
  * Date:       10th November 2011
- *  
+ *
  **/
 
 
 #include <iostream>
 #include <fstream>
 #include <libplayerc++/playerc++.h>
-using namespace PlayerCc;  
+using namespace PlayerCc;
 
 
 /**
@@ -29,150 +29,150 @@ void printRobotData(BumperProxy& bp, player_pose2d_t pose);
 void printLaserData(LaserProxy& sp);
 
 int  readPlanLength(void);
-void readPlan(double *, int);
-void printPlan(double *,int);  
-void writePlan(double *, int);
+void readPlan(double*, int);
+void printPlan(double*, int);
+void writePlan(double*, int);
 
 /**
  * main()
  *
  **/
 
-int main(int argc, char *argv[])
-{  
+int main(int argc, char* argv[])
+{
 
-  // Variables
-  int counter = 0;
-  double speed;            // How fast do we want the robot to go forwards?
-  double turnrate;         // How fast do we want the robot to turn?
-  player_pose2d_t  pose;   // For handling localization data
+	// Variables
+	int counter = 0;
+	double speed;            // How fast do we want the robot to go forwards?
+	double turnrate;         // How fast do we want the robot to turn?
+	player_pose2d_t  pose;   // For handling localization data
 
-  // The set of coordinates that makes up the plan
+	// The set of coordinates that makes up the plan
 
-  int pLength;
-  double *plan;
+	int pLength;
+	double* plan;
 
-  // Set up proxies. These are the names we will use to connect to 
-  // the interface to the robot.
-  PlayerClient    robot("localhost");  
-  BumperProxy     bp(&robot,0);  
-  Position2dProxy pp(&robot,0);
-  LocalizeProxy   lp (&robot, 0);
-  LaserProxy      sp (&robot, 0);
-  
-  // Allow the program to take charge of the motors (take care now)
-  pp.SetMotorEnable(true);
+	// Set up proxies. These are the names we will use to connect to 
+	// the interface to the robot.
+	PlayerClient    robot("localhost");
+	BumperProxy     bp(&robot, 0);
+	Position2dProxy pp(&robot, 0);
+	LocalizeProxy   lp(&robot, 0);
+	LaserProxy      sp(&robot, 0);
 
-  // Plan handling
-  // 
-  // A plan is an integer, n, followed by n doubles (n has to be
-  // even). The first and second doubles are the initial x and y
-  // (respectively) coordinates of the robot, the third and fourth
-  // doubles give the first location that the robot should move to, and
-  // so on. The last pair of doubles give the point at which the robot
-  // should stop.
-  pLength = readPlanLength(); // Find out how long the plan is from plan.txt
-  plan = new double[pLength]; // Create enough space to store the plan
-  readPlan(plan, pLength);    // Read the plan from the file plan.txt.
-  printPlan(plan,pLength);    // Print the plan on the screen
-  writePlan(plan, pLength);   // Write the plan to the file plan-out.txt
+	// Allow the program to take charge of the motors (take care now)
+	pp.SetMotorEnable(true);
+
+	// Plan handling
+	// 
+	// A plan is an integer, n, followed by n doubles (n has to be
+	// even). The first and second doubles are the initial x and y
+	// (respectively) coordinates of the robot, the third and fourth
+	// doubles give the first location that the robot should move to, and
+	// so on. The last pair of doubles give the point at which the robot
+	// should stop.
+	pLength = readPlanLength(); // Find out how long the plan is from plan.txt
+	plan = new double[pLength]; // Create enough space to store the plan
+	readPlan(plan, pLength);    // Read the plan from the file plan.txt.
+	printPlan(plan, pLength);    // Print the plan on the screen
+	writePlan(plan, pLength);   // Write the plan to the file plan-out.txt
 
 
-  // Main control loop
-  while(true) 
-    {    
-      // Update information from the robot.
-      robot.Read();
-      // Read new information about position
-      pose = readPosition(lp);
-      // Print data on the robot to the terminal
-      printRobotData(bp, pose);
-      // Print information about the laser. Check the counter first to stop
-      // problems on startup
-      if(counter > 2){
-	printLaserData(sp);
-      }
+	// Main control loop
+	while (true)
+	{
+		// Update information from the robot.
+		robot.Read();
+		// Read new information about position
+		pose = readPosition(lp);
+		// Print data on the robot to the terminal
+		printRobotData(bp, pose);
+		// Print information about the laser. Check the counter first to stop
+		// problems on startup
+		if (counter > 2) {
+			printLaserData(sp);
+		}
 
-      // Print data on the robot to the terminal --- turned off for now.
-      // printRobotData(bp, pose);
-      
-      // If either bumper is pressed, stop. Otherwise just go forwards
+		// Print data on the robot to the terminal --- turned off for now.
+		// printRobotData(bp, pose);
 
-      if(bp[0] || bp[1]){
-	speed= 0;
-	turnrate= 0;
-      } 
-      else {
-	speed=.1;
-        turnrate = 0;
-      }     
+		// If either bumper is pressed, stop. Otherwise just go forwards
 
-      // What are we doing?
-      std::cout << "Speed: " << speed << std::endl;      
-      std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
+		if (bp[0] || bp[1]) {
+			speed = 0;
+			turnrate = 0;
+		}
+		else {
+			speed = .1;
+			turnrate = 0;
+		}
 
-      // Send the commands to the robot
-      pp.SetSpeed(speed, turnrate);  
-      // Count how many times we do this
-      counter++;
-    }
-  
+		// What are we doing?
+		std::cout << "Speed: " << speed << std::endl;
+		std::cout << "Turn rate: " << turnrate << std::endl << std::endl;
+
+		// Send the commands to the robot
+		pp.SetSpeed(speed, turnrate);
+		// Count how many times we do this
+		counter++;
+	}
+
 } // end of main()
 
 /**
  * readPosition()
  *
- * Read the position of the robot from the localization proxy. 
+ * Read the position of the robot from the localization proxy.
  *
  * The localization proxy gives us a hypothesis, and from that we extract
- * the mean, which is a pose. 
+ * the mean, which is a pose.
  *
  **/
 
 player_pose2d_t readPosition(LocalizeProxy& lp)
 {
 
-  player_localize_hypoth_t hypothesis;
-  player_pose2d_t          pose;
-  uint32_t                 hCount;
+	player_localize_hypoth_t hypothesis;
+	player_pose2d_t          pose;
+	uint32_t                 hCount;
 
-  // Need some messing around to avoid a crash when the proxy is
-  // starting up.
+	// Need some messing around to avoid a crash when the proxy is
+	// starting up.
 
-  hCount = lp.GetHypothCount();
+	hCount = lp.GetHypothCount();
 
-  if(hCount > 0){
-    hypothesis = lp.GetHypoth(0);
-    pose       = hypothesis.mean;
-  }
+	if (hCount > 0) {
+		hypothesis = lp.GetHypoth(0);
+		pose = hypothesis.mean;
+	}
 
-  return pose;
+	return pose;
 } // End of readPosition()
 
 
 void printLaserData(LaserProxy& sp)
 {
 
-  double maxRange, minLeft, minRight, range, bearing;
-  int points;
+	double maxRange, minLeft, minRight, range, bearing;
+	int points;
 
-  maxRange  = sp.GetMaxRange();
-  minLeft   = sp.MinLeft();
-  minRight  = sp.MinRight();
-  range     = sp.GetRange(5);
-  bearing   = sp.GetBearing(5);
-  points    = sp.GetCount();
+	maxRange = sp.GetMaxRange();
+	minLeft = sp.MinLeft();
+	minRight = sp.MinRight();
+	range = sp.GetRange(5);
+	bearing = sp.GetBearing(5);
+	points = sp.GetCount();
 
-  //Uncomment this to print out useful laser data
-  //std::cout << "Laser says..." << std::endl;
-  //std::cout << "Maximum distance I can see: " << maxRange << std::endl;
-  //std::cout << "Number of readings I return: " << points << std::endl;
-  //std::cout << "Closest thing on left: " << minLeft << std::endl;
-  //std::cout << "Closest thing on right: " << minRight << std::endl;
-  //std::cout << "Range of a single point: " << range << std::endl;
-  //std::cout << "Bearing of a single point: " << bearing << std::endl;
+	//Uncomment this to print out useful laser data
+	//std::cout << "Laser says..." << std::endl;
+	//std::cout << "Maximum distance I can see: " << maxRange << std::endl;
+	//std::cout << "Number of readings I return: " << points << std::endl;
+	//std::cout << "Closest thing on left: " << minLeft << std::endl;
+	//std::cout << "Closest thing on right: " << minRight << std::endl;
+	//std::cout << "Range of a single point: " << range << std::endl;
+	//std::cout << "Bearing of a single point: " << bearing << std::endl;
 
-  return;
+	return;
 } // End of printLaserData()
 
 /**
@@ -186,19 +186,19 @@ void printLaserData(LaserProxy& sp)
 void printRobotData(BumperProxy& bp, player_pose2d_t pose)
 {
 
-  // Print out what the bumpers tell us:
-  std::cout << "Left  bumper: " << bp[0] << std::endl;
-  std::cout << "Right bumper: " << bp[1] << std::endl;
-  // Can also print the bumpers with:
-  //std::cout << bp << std::endl;
+	// Print out what the bumpers tell us:
+	std::cout << "Left  bumper: " << bp[0] << std::endl;
+	std::cout << "Right bumper: " << bp[1] << std::endl;
+	// Can also print the bumpers with:
+	//std::cout << bp << std::endl;
 
-  // Print out where we are
-  std::cout << "We are at" << std::endl;
-  std::cout << "X: " << pose.px << std::endl;
-  std::cout << "Y: " << pose.py << std::endl;
-  std::cout << "A: " << pose.pa << std::endl;
+	// Print out where we are
+	std::cout << "We are at" << std::endl;
+	std::cout << "X: " << pose.px << std::endl;
+	std::cout << "Y: " << pose.py << std::endl;
+	std::cout << "A: " << pose.pa << std::endl;
 
-  
+
 } // End of printRobotData()
 
 /**
@@ -211,21 +211,21 @@ void printRobotData(BumperProxy& bp, player_pose2d_t pose)
 
 int readPlanLength(void)
 {
-  int length;
+	int length;
 
-  std::ifstream planFile;
-  planFile.open("plan.txt");
+	std::ifstream planFile;
+	planFile.open("plan.txt");
 
-  planFile >> length;
-  planFile.close();
+	planFile >> length;
+	planFile.close();
 
-  // Some minimal error checking
-  if((length % 2) != 0){
-    std::cout << "The plan has mismatched x and y coordinates" << std::endl;
-    exit(1);
-  }
+	// Some minimal error checking
+	if ((length % 2) != 0) {
+		std::cout << "The plan has mismatched x and y coordinates" << std::endl;
+		exit(1);
+	}
 
-  return length;
+	return length;
 
 } // End of readPlanLength
 
@@ -237,19 +237,19 @@ int readPlanLength(void)
  *
  **/
 
-void readPlan(double *plan, int length)
+void readPlan(double* plan, int length)
 {
-  int skip;
+	int skip;
 
-  std::ifstream planFile;
-  planFile.open("plan.txt");
+	std::ifstream planFile;
+	planFile.open("plan.txt");
 
-  planFile >> skip;
-  for(int i = 0; i < length; i++){
-    planFile >> plan[i];
-  }
+	planFile >> skip;
+	for (int i = 0; i < length; i++) {
+		planFile >> plan[i];
+	}
 
-  planFile.close();
+	planFile.close();
 
 } // End of readPlan
 
@@ -261,40 +261,40 @@ void readPlan(double *plan, int length)
  *
  **/
 
-void printPlan(double *plan , int length)
+void printPlan(double* plan, int length)
 {
-  std::cout << std::endl;
-  std::cout << "   x     y" << std::endl;
-  for(int i = 0; i < length; i++){
-    std::cout.width(5);
-    std::cout << plan[i] << " ";
-    if((i > 0) && ((i % 2) != 0)){
-      std::cout << std::endl;
-    }
-  }
-  std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "   x     y" << std::endl;
+	for (int i = 0; i < length; i++) {
+		std::cout.width(5);
+		std::cout << plan[i] << " ";
+		if ((i > 0) && ((i % 2) != 0)) {
+			std::cout << std::endl;
+		}
+	}
+	std::cout << std::endl;
 
 } // End of printPlan
 
 
 /**
  * writePlan
- * 
+ *
  * Send the plan to the file plan-out.txt, preceeded by the information
  * about how long it is.
  *
  **/
 
-void writePlan(double *plan , int length)
+void writePlan(double* plan, int length)
 {
-  std::ofstream planFile;
-  planFile.open("plan-out.txt");
+	std::ofstream planFile;
+	planFile.open("plan-out.txt");
 
-  planFile << length << " ";
-  for(int i = 0; i < length; i++){
-    planFile << plan[i] << " ";
-  }
+	planFile << length << " ";
+	for (int i = 0; i < length; i++) {
+		planFile << plan[i] << " ";
+	}
 
-  planFile.close();
+	planFile.close();
 
 } // End of writePlan
